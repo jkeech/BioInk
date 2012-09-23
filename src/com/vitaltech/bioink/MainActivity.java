8f5c67e6c24d810b072836c9c6de592068747336
John Keech <jkeech@live.com>
2012-09-23 17:46:39 -0500

    Fixed some project settings

--- src/com/vitaltech/bioink/MainActivity.java
+++ src/com/vitaltech/bioink/MainActivity.java
@@ -2,6 +2,7 @@ package com.vitaltech.bioink;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 
 // master
@@ -12,6 +13,7 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Log.e("ErrorType","Error msg here");
     }
 
     @Override
