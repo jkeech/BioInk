53121f960aa0c8d5d2dd5562cb95f100aeaab9ca
John Keech <jkeech@live.com>
2012-09-24 00:33:26 -0500

    Added JUnit test class for the Scene

--- src/com/vitaltech/bioink/Scene.java
+++ src/com/vitaltech/bioink/Scene.java
@@ -4,14 +4,14 @@ package com.vitaltech.bioink;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class Scene {
-	private ConcurrentHashMap<String,User> users = new ConcurrentHashMap<String,User>(7);
+	public ConcurrentHashMap<String,User> users = new ConcurrentHashMap<String,User>(7);
 	
 	/*
 	 * This method updates a certain DataType value for one user. The user object
 	 * will be created if it does not exist. This method should be called by the
 	 * data processing module.
 	 */
-	void update(String id, DataType type, double val){
+	public void update(String id, DataType type, double val){
 		users.putIfAbsent(id,new User()); // insert into the dictionary if it does not exist
 		
 		// update the user in the scene
---
+++ test/com/vitaltech/bioink/SceneTest.java
@@ -0,0 +1,27 @@
+package com.vitaltech.bioink;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SceneTest {
+
+	@Test
+	public void testUpdate() {
+		Scene scene = new Scene();
+		
+		// add some dummy items
+		scene.update("user1",DataType.HEARTRATE,50);
+		scene.update("user2",DataType.TEMP,98.6);
+		scene.update("user1",DataType.TEMP,99.78);
+		
+		assertEquals(50,scene.users.get("user1").heartrate,0);
+		assertEquals(99.78,scene.users.get("user1").temp,0);
+		assertEquals(98.6,scene.users.get("user2").temp,0 );
+		
+		// update existing item
+		scene.update("user1",DataType.HEARTRATE,60.3);
+		assertEquals(60.3,scene.users.get("user1").heartrate,0);
+	}
+
+}
