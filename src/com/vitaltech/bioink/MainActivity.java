de87d102d2879559605f6056eac84227a41c3997
beef <met898@tamu.edu>
2012-09-24 23:57:19 -0500

    Live viz button runs sample toast.

--- src/com/vitaltech/bioink/MainActivity.java
+++ src/com/vitaltech/bioink/MainActivity.java
@@ -4,17 +4,31 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 // user interface
 
 public class MainActivity extends Activity {
 	private static final String TAG = MainActivity.class.getSimpleName();
+	private Button vizButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "Main Activity created");
         setContentView(R.layout.activity_main);
+        this.vizButton=(Button)this.findViewById(R.id.button1);
+        this.vizButton.setOnClickListener(
+        	new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					Toast.makeText(getApplicationContext(), "start viz pressed", Toast.LENGTH_SHORT).show();
+				}
+			}
+		);
     }
 
     @Override
