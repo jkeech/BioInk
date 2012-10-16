3368641061c58d237df6d1315af10e8053ca9df6
marioc <nmcastro1@gmail.com>
2012-10-16 17:37:33 -0500

    Added respiration and heart rate mappigs, provitional positional mapping function

--- src/com/vitaltech/bioink/DataProcess.java
+++ src/com/vitaltech/bioink/DataProcess.java
@@ -1,99 +1,102 @@
 package com.vitaltech.bioink;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ArrayList;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class DataProcess {
 	//List of current users
-	public List<UserDP> users;
+	public ConcurrentHashMap<String,User> users;
+	private Scene scene;
 	
-	public DataProcess(){
-		users = new ArrayList<UserDP>();
+	//Positioning range constants
+	private final float minPos = -10;
+	private final float maxPos = 10;
+	
+	//Heart rate range constant
+	private final float minHR = 0;
+	private final float maxHR = 250;
+	
+	//Respiration rate 
+	private final float minResp = 0;
+	private final float maxResp = 70;
+	
+	//Data Processing constructor
+	public DataProcess(Scene dscene){
+		users = new ConcurrentHashMap<String,User>(13);
+		
+		this.scene = dscene;
 	}
 	
-	//Add user to the user list
-	public void addUser(String userID){
-		users.add(new UserDP(userID));	
+	//flag that controls whether or not the first HRV has been calculated
+	public boolean active_hrv = false;
+	
+	//Method allows bluetooth mod to push data into data Process mod
+	//User is specified by its ID
+	public void push(String uid, BiometricType dtype, float value){
+		if(!users.containsKey(uid)){
+			User tmp = new User();
+			users.put(uid,tmp); // insert into the dictionary if it does not exist
+			
+		}
+		
+		switch(dtype){
+		case HEARTRATE: 
+			users.get(uid).heartrate = value;
+			break;
+		case RESPIRATION:
+			users.get(uid).respiration = value;
+			break;
+		case RR: 
+			//To be added once the RR interval structures have been built
+			break;
+		default:
+			break;
+		}
+		
 	}
 	
-	//Push user data into data processing module, user is specified by its ID
-	public void push(String userID, DataType dtype, float value){
-		Iterator<UserDP> it = users.iterator();
+	//map respiration rate from [minhr, maxhr] to [0, 1]
+	public void scaleHeartRate(String uid){
+		float temp = 0;
+		temp = (users.get(uid).heartrate - minHR ) / (maxHR - minHR);
 		
-		while(it.hasNext()){
-			UserDP user = it.next();
-			if(user.id.equals(userID)){
-				switch(dtype){
-				case HEARTRATE: 
-					user.heartrate = value; 
-					break;
-				case RESPIRATION: 
-					user.respiration = value;
-					break;
-				case TEMP: 
-					user.temp = value; 
-					break;
-				case CONDUCTIVITY: 
-					user.conductivity = value;
-					break;
-				default:
-					break;
-				}
-			}
-		}
+		scene.update(uid, DataType.ENERGY, temp);
 	}
 	
-	public UserDP getUser(String userID){
-		Iterator<UserDP> it = users.iterator();
+	//map respiration rate from [minresp, maxresp] to [0, 1]
+	public void scaleRespirationRate(String uid){
+		float temp = 0;
+		temp = (users.get(uid).respiration - minResp ) / (maxResp - minResp);
 		
-		while(it.hasNext()){
-			UserDP user = it.next();
-			if(user.id.equals(userID)){
-				return user;
-			}
-		}
-		return null;
+		scene.update(uid, DataType.COLOR, temp);
 	}
 	
-	public float userDistance(String id_x, String id_y){
-		float diff = 0;
-		float total = 0;
+	//map position of given biometrics from their own intervals to [minpos, maxpos]
+	//Note: the current biometric values are being mapped into a cube of side length maxpos - minpos
+	//aka not a sphere yet
+	public void userPosition(String uid){
+		float temp = 0;
+		float x = 0;
+		float y = 0;
+		float z = 0;
 		
-		UserDP x = getUser(id_x);
-		UserDP y = getUser(id_y);
+		//extract user heart rate and calculate x-axis positioning
+		temp = users.get(uid).heartrate;
+		x = (temp - minHR ) / (maxHR - minHR);
+		x = x * (maxPos - minPos);
+		x = x + minPos;
 		
-		if(x.heartrate > y.heartrate){
-			diff = x.heartrate - y.heartrate;
-			total += diff;
-		}else{
-			diff = y.heartrate - x.heartrate;
-			total += diff;
+		//extract user respiration rate and calculate y-axis positioning
+		temp = users.get(uid).respiration;
+		y = (temp - minResp ) / (maxResp - minResp);
+		y = y * (maxPos - minPos);
+		y = y + minPos;
+		
+		//update x and y values
+		scene.update(uid, DataType.X, x);
+		scene.update(uid, DataType.Y, y);
+		
+		if(active_hrv){
+			//calculate and update Z position value
+			z = 1;
 		}
-		
-		if(x.respiration > y.respiration){
-			diff = x.respiration - y.respiration;
-			total += diff;
-		}else{
-			diff = y.respiration - x.respiration;
-			total += diff;
-		}
-		
-		if(x.temp > y.temp){
-			diff = x.temp - y.temp;
-			total += diff;
-		}else{
-			diff = y.temp - x.temp;
-			total += diff;
-		}
-		
-		if(x.conductivity > y.conductivity){
-			diff = x.conductivity - y.conductivity;
-			total += diff;
-		}else{
-			diff = y.conductivity - x.conductivity;
-			total += diff;
-		}
-		
-		return total;
 	}
 }
