95ce301b166554bc6c50201736938a2ec4f4db06
beef <met898@tamu.edu>
2012-10-20 15:24:32 -0500

    I'm pretty sure this works.

--- src/BitOps.java
+++ src/BitOps.java
@@ -19,33 +19,33 @@ public class BitOps {
 	public static void main(String[] args) {
 		Set<Byte> bytes = new LinkedHashSet<Byte>();
 		
-		bytes.add((byte)  1);	// 0000 0001 => 00000 00001 1	 1
-		bytes.add((byte)  4);	// 0000 0100 => 00000 00001 1	 2
-		bytes.add((byte) 16);	// 0001 0000 => 00000 00001 1	 3
-		bytes.add((byte) 64);	// 0100 0000 => 00000 00001 1	 4
-		bytes.add((byte)  0);	// 0000 0000
+		bytes.add((byte)  1);	// 0000 0001 => 00000 00001 =    1		 1
+		bytes.add((byte)  4);	// 0000 0100 => 00000 00001 =    1		 2
+		bytes.add((byte) 16);	// 0001 0000 => 00000 00001 =    1		 3
+		bytes.add((byte) 64);	// 0100 0000 => 00000 00001 =    1		 4
+		bytes.add((byte)  0);	// 0000 0000 ^
 
-		bytes.add((byte)  3);	// 0000 0011 => 11000 00011 771	 5
-		bytes.add((byte) 31);	// 0001 1111 => 11100 00111 903	 6
-		bytes.add((byte)238);	// 1110 1110 => 01110 01110 462	 7
-		bytes.add((byte) 28);	// 0001 1100 => 00111 11100 252	 8
-		bytes.add((byte) 63);	// 0011 1111
+		bytes.add((byte)  3);	// 0000 0011 => 11000 00011 =  771		 5
+		bytes.add((byte) 31);	// 0001 1111 => 11100 00111 =  903		 6
+		bytes.add((byte)238);	// 1110 1110 => 01110 01110 =  462		 7
+		bytes.add((byte) 28);	// 0001 1100 => 00111 11100 =  252		 8
+		bytes.add((byte) 63);	// 0011 1111 ^
 
-		bytes.add((byte) 48);	// 0011 0000 => 10001 10000 560	 9
-		bytes.add((byte)  2);	// 0000 0010 => 10000 00000 512	10
-		bytes.add((byte)  8);	// 0000 1000 => 10000 00000 512	11
-		bytes.add((byte) 32);	// 0010 0000 => 10000 00000 512	12
-		bytes.add((byte)128);	// 1000 0000
+		bytes.add((byte) 48);	// 0011 0000 => 10001 10000 =  560		 9
+		bytes.add((byte)  2);	// 0000 0010 => 10000 00000 =  512		10
+		bytes.add((byte)  8);	// 0000 1000 => 10000 00000 =  512		11
+		bytes.add((byte) 32);	// 0010 0000 => 10000 00000 =  512		12
+		bytes.add((byte)128);	// 1000 0000 ^
 
-		bytes.add((byte)255);	// 1111 1111 => 11111 11111 1023	13
-		bytes.add((byte)195);	// 1100 0011 => 00001 10000 48	14
-		bytes.add((byte)240);	// 1111 0000 => 11111 01111 1007	15
-		bytes.add((byte) 62);	// 0011 1110 => 00111 10100 244	16
-		bytes.add((byte) 61);	// 0011 1101
+		bytes.add((byte)255);	// 1111 1111 => 11111 11111 = 1023		13
+		bytes.add((byte)195);	// 1100 0011 => 00001 10000 =   48		14
+		bytes.add((byte)240);	// 1111 0000 => 11111 01111 = 1007		15
+		bytes.add((byte) 62);	// 0011 1110 => 00111 10100 =  244		16
+		bytes.add((byte) 61);	// 0011 1101 ^
 
-		bytes.add((byte) 85);	// 0101 0101 => 01010 10101 341	17
-		bytes.add((byte)169);	// 1010 1001 => 10101 01010 682	18 samples in 23 bytes
-		bytes.add((byte)106);	// 0110 1010 =>	 junk  0110
+		bytes.add((byte) 85);	// 0101 0101 => 01010 10101 = 341		17
+		bytes.add((byte)169);	// 1010 1001 => 10101 01010 = 682		18 samples in 22.5 bytes
+		bytes.add((byte)106);	// 0110 1010 ^	(junk 0110)
 
 //		for(int i=0; i<8; ++i){
 //			System.out.println("\ni=" + i + " => " + (0x1 << i) + "\t 2^ => " + 
@@ -95,29 +95,27 @@ public class BitOps {
 	
 	public static List<Long> zephyParse(Set<Byte> samples) {
 		List<Long> decoded = new ArrayList<Long>();
-//		Byte[] array = (Byte[]) samples.toArray();
-
 		Byte[] array = (Byte[])samples.toArray(new Byte[samples.size()]);
 
 		int shifter = 1;
 		for(int i=1; i<samples.size(); ++i){
-			long rightMask = (255 << ((shifter-1) * 2)) & 255;
-			long right = (((long)array[i-1]) & rightMask) >> ((shifter-1)*2);
-			long leftMask = (1 << (shifter * 2)) - 1;
-			long left = ((((long)array[i]) & leftMask) << (8-((shifter-1)*2)));
-			
 			if(shifter != 5){
+				long rightMask = (255 << ((shifter-1) * 2)) & 255;
+				long right = (((long)array[i-1]) & rightMask) >> ((shifter-1)*2);
+				long leftMask = (1 << (shifter * 2)) - 1;
+				long left = ((((long)array[i]) & leftMask) << (8-((shifter-1)*2)));
+				
 				decoded.add(
 	//				Integer.valueOf(
 						(left | right) & 1023
 	//				)
 				);
+
+//			System.out.println(shifter + " ," + (long)array[i-1] + ", " + (long)array[i] + "\t :: " + 
+//					BigInteger.valueOf(left).toString(2) + "\t & " + 
+//					BigInteger.valueOf(right).toString(2) + "\t >> " + decoded.get(decoded.size()-1));
+
 			}
-
-			System.out.println(shifter + " ," + (long)array[i-1] + ", " + (long)array[i] + "\t :: " + 
-					BigInteger.valueOf(left).toString(2) + "\t & " + 
-					BigInteger.valueOf(right).toString(2) + "\t >> " + decoded.get(decoded.size()-1));
-
 			shifter++;
 			if(shifter > 5){
 				shifter = 1;
