baba837ec81e3252e35f8a5fb53f416241dacdf9
marioc <nmcastro1@gmail.com>
2012-11-04 16:06:35 -0600

    fixed merging bug

--- src/com/vitaltech/bioink/RajActivity.java
+++ src/com/vitaltech/bioink/RajActivity.java
@@ -72,7 +72,7 @@ public class RajActivity extends RajawaliActivity {
 		}
 		dataSim = new Thread(new Runnable() {
 			public void run() {
-				new DataSimulatorDP(dp).run();
+				new DataSimulatorPlus(dp).run();
 			}
 		});// debug data
 		dataSim.start();
